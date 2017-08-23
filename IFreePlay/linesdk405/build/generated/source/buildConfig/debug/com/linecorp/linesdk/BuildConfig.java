00d938fcceac4214f1f4c38ef93f5043503e6669
gaowei <gaowei_yb@163.com>
2017-08-23 10:06:00 +0800

    update2

---
+++ IFreePlay/linesdk405/build/generated/source/buildConfig/debug/com/linecorp/linesdk/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.linecorp.linesdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.linecorp.linesdk";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 40005;
+  public static final String VERSION_NAME = "4.0.5";
+}
